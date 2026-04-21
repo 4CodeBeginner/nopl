aa3723af13d2e89402ad97c106987947e6f03678
Nopal <141114024+4CodeBeginner@users.noreply.github.com>
2026-04-21 19:44:06 +0700

    Initial commit

---
+++ database/migrations/2026_04_16_070045_create_products_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->integer('id_product')->nullable(); // opsional kalau masih mau pakai
+            $table->string('name_product');
+            $table->enum('brand', ['hotw', 'minigt', 'poprace', 'tomica', 'mbx']);
+            $table->text('description')->nullable();
+            $table->text('photo')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
---
+++ database/migrations/2026_04_16_070052_create_sales_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up()
+    {
+        Schema::create('sales', function (Blueprint $table) {
+            $table->id();
+            $table->dateTime('sale_date')->useCurrent();
+            $table->string('customer_name')->nullable();
+            $table->string('tracking_number')->nullable();
+            $table->decimal('total_amount', 12, 2)->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('sales');
+    }
+};
