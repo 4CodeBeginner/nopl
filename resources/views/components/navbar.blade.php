4458f5056c29790d65bc030cfe2d2816da6c7d3f
ZefriSN <zefrinata@gmail.com>
2026-04-25 17:58:31 +0700

    bikin navbar + halaman

---
+++ resources/views/components/navbar.blade.php
@@ -0,0 +1,15 @@
+<nav class="navbar">
+    <div class="nav-left">
+        <div>
+            <img class="logo" src="img/emstoys.png" alt="logo">
+        </div>
+    
+
+        <div class="nav-menu">
+            <a href="/"> Home </a>
+            <a href="/product"> Product </a>
+            <a href='/about'> About Us </a>
+            <a href='/contact'> Contact Us </a>
+        </div>
+    </div>
+</nav>
---
+++ resources/views/pages/product.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Product</title>
+</head>
+<body>
+
+    @extends('layout.app')
+    @section('content')
+
+  <h1> product </h1>
+    @endsection
+
+</body>
+</html>
